6f52987587fd354d7c0bbe47edd1a219191c07ef
Eddie Larabi <117444690+SElarabi@users.noreply.github.com>
2024-03-25 18:21:04 -0400

    fix sync scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,7 @@ export default function RootLayout({
 					src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'
 					integrity='sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4'
 					crossOrigin='anonymous'
+					async
 				></script>
 			</body>
 		</html>
